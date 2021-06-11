32126918f6530b08a116c1219950d6a0b7a34323
Yaseen Maqsudi <yaseenmaqsudi@gmail.com>
2021-06-11 11:51:46 -0400

    my initial commit

---
+++ src/scenes/PauseScene.js
@@ -0,0 +1,45 @@
+import BaseScene from './BaseScene'
+
+class PauseScene extends BaseScene {
+    constructor(config) {
+        super('PauseScene', config);
+
+        this.menu = [
+            {scene: 'PlayScene', text: 'Continue'},
+            {scene: 'MenuScene', text: 'Exit'},
+        ]
+    }
+
+    create() {
+        super.create();
+        this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+    }
+
+    setupMenuEvents(menuItem) {
+        const textGO = menuItem.textGO;
+        textGO.setInteractive();
+
+        textGO.on('pointerover', () => {
+            textGO.setStyle({fill: '#ff0'});
+        })
+
+
+        textGO.on('pointerout', () => {
+            textGO.setStyle({fill: '#fff'});
+        })
+
+        textGO.on('pointerup', () => {
+            // shutting down pause scene and resuming the play scene
+            if (menuItem.scene && menuItem.text === 'Continue') {
+                this.scene.stop();
+                this.scene.resume(menuItem.scene);
+            } else {
+                // shutting down playscene and pausescene and running menu
+                this.scene.stop('PlayScene');
+                this.scene.start('MenuScene');
+            }
+        })
+    }
+}
+
+export default PauseScene;
